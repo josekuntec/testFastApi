26f2d14967967318e25469bd2398c1b0729cc7f9
Jose Angel Martínez Navarro <144565832+josekuntec@users.noreply.github.com>
2025-03-10 11:09:45 -0600

    Update main.py
    suma added

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class FormData(BaseModel):
+    primero: str
+    segundo: str
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/suma")
+def suma(item: Annotated[FormData, Form()]):
+    x = int(item.primero)
+    y = int(item.segundo)
+    resultado = x+y
+    return str(resultado)
